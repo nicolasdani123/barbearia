e0f285c860ef5ee5547088ca1519a440eb37ac28
Nicolas Daniel Santo <ppssppvst1935@gmail.com>
2025-10-31 14:46:29 -0300

    Configuração completa para deploy no Render

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const db = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
